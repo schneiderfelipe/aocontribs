6d2b720aa937cb3fe3fd6cd4f69b48fd6cd52987
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2022-04-11 18:24:30 -0300

    Transfer project

---
+++ src/aocontribs/__init__.py
@@ -0,0 +1,31 @@
+import sys
+
+import cclib
+
+__version__ = "0.1.0"
+
+
+def cli(filepath: str, atom: str):
+    data = cclib.io.ccopen(filepath).parse()
+
+    ihomo = data.homos[0]
+    homo_alpha = data.mocoeffs[0][ihomo, :]
+    print(f"HOMO(α): #{ihomo}")
+    print("Only non-zero contributions shown")
+
+    contribs = []
+    for (i, orbital) in enumerate(data.aonames):
+        if orbital.startswith(atom):
+            coeff = homo_alpha[i]
+            contribs.append((orbital, i, coeff))
+
+    contribs = sorted(contribs, key=lambda e: -e[2] ** 2)
+    for (orbital, i, coeff) in contribs:
+        if coeff**2 > 0:
+            print(f"{orbital:10s} (#{i:2d}): " f"({coeff:+1.4f})² = {coeff**2:1.4f}")
+
+
+if __name__ == "__main__":
+    filepath = sys.argv[1]
+    atom = sys.argv[2]
+    cli(filepath, atom)
