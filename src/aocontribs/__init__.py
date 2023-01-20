184b25c6436206d1cc0b03043685b51368e4e3d2
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-01-20 14:21:40 -0300

    feat: analyse MOs other than the HOMO

--- src/aocontribs/__init__.py
+++ src/aocontribs/__init__.py
@@ -4,38 +4,59 @@ import cclib
 
 __version__ = "0.1.0"
 
+THRESHOLD = 0.01
 
-def main(filepath: str, atom: str):
+
+def main(filepath: str, atom: str, n: int):
     data = cclib.io.ccopen(filepath).parse()
+    ihomo = data.homos[0] + n
 
-    ihomo = data.homos[0]
+    imax = len(data.moenergies[0]) - 1
+    if not (0 <= ihomo <= imax):
+        raise ValueError(
+            f"Attempt to index MO #{ihomo} when only {0}-{imax} (inclusive) available"
+        )
+
     homo_alpha = data.mocoeffs[0][ihomo, :]
-    print(f"HOMO(α): #{ihomo}")
-    print("Only non-zero contributions shown")
+    if n == 0:
+        print(f"HOMO(α): #{ihomo}")
+    elif n == 1:
+        print(f"LUMO(α): #{ihomo}")
+    elif n > 1:
+        print(f"LUMO{n-1:+}(α): #{ihomo}")
+    else:
+        print(f"HOMO{n:+}(α): #{ihomo}")
+    print(f"Only contributions greater than {THRESHOLD} shown")
 
     contribs = []
     for (i, orbital) in enumerate(data.aonames):
         if orbital.startswith(atom):
             coeff = homo_alpha[i]
-            contribs.append((orbital, i, coeff))
+            contribs.append((orbital, i, coeff, coeff**2))
 
-    contribs = sorted(contribs, key=lambda e: -e[2] ** 2)
-    for (orbital, i, coeff) in contribs:
-        if coeff**2 > 0:
-            print(f"{orbital:10s} (#{i:2d}): " f"({coeff:+1.4f})² = {coeff**2:1.4f}")
+    contribs = sorted(contribs, key=lambda e: -e[3])
+    for (orbital, i, coeff, p) in contribs:
+        if p > THRESHOLD:
+            print(f"{orbital:10s} (#{i:2d}): " f"({coeff:+1.4f})² = {p:1.4f}")
 
 
 def cli():
-    if len(sys.argv) != 3:
+    if len(sys.argv) not in (3, 4):
         progname = sys.argv[0].split("/")[-1]
-        print(f"Usage: {progname} <filepath> <atom>")
-        print("\nExample:")
-        print(f"  {progname} test/water.out O1")
+        print(f"Usage: {progname} <filepath> <atom> [<n>]")
+        print("\nExamples:")
+        print(f"  {progname} test/water.out O1     # HOMO by default")
+        print(f"  {progname} test/water.out O1 +1  # LUMO")
+        print(f"  {progname} test/water.out O1 -1  # HOMO-1")
         sys.exit(1)
 
     filepath = sys.argv[1]
     atom = sys.argv[2]
-    main(filepath, atom)
+    if len(sys.argv) > 3:
+        n = int(sys.argv[3])
+    else:
+        n = 0
+    main(filepath, atom, n)
 
 
 if __name__ == "__main__":
